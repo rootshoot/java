dc2e0a8bcea53ace36b17cba96e016a6cef0883f
Suraj Patil <54897129+Suraj1912@users.noreply.github.com>
2020-10-02 21:17:17 +0530

    Update main.java

--- main.java
+++ main.java
@@ -3,4 +3,8 @@ public static void main(Strings []args)
 {
 System.out.println("Hello Vansh");
   System.out.println("Hello Samaksh");
+  print();
+}
+static void print(){
+System.out.println("Hello");
 }
